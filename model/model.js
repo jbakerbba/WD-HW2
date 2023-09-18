431e5248d7da30141f08e0090078b303aa4f8448
jbakerbba <jbakerbba2@gmail.com>
2023-09-18 00:33:25 -0400

    first commit

---
+++ model/model.js
@@ -0,0 +1,57 @@
+//model
+var loggedInOut = false;
+var homeContent =  `<h1>Home Page</h1>
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error in ex minus explicabo sed tempore, doloribus reiciendis consequatur nihil ut, quis ullam, cumque rerum veniam. Ad alias facilis vero incidunt.
+Incidunt tenetur doloribus eveniet deserunt optio tempora harum, dolorem quisquam itaque molestias iusto illum vel doloremque quia facere officiis quos natus, provident sed? Dicta ipsa, libero id ducimus quod vitae.
+Natus ea corporis amet necessitatibus suscipit quis eos? At dolores vel dolorum veritatis distinctio fuga itaque, tempora repellendus aliquid aliquam non voluptas doloribus molestias? Repudiandae harum ratione aperiam necessitatibus nisi?`;
+var aboutContent =  `<h1>About Page</h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique labore laudantium obcaecati recusandae inventore harum, tempore officia itaque placeat velit aspernatur cum adipisci facilis nulla sint tempora autem deserunt maxime.
+Porro reiciendis veniam adipisci, dolorem sequi eveniet, quod ipsa minus illo delectus alias recusandae perspiciatis error impedit commodi natus expedita. Iste quis numquam, sapiente labore exercitationem distinctio nemo voluptates corporis.
+Incidunt quos fugit laborum, rerum soluta eum id sequi nihil numquam quam explicabo a ipsum ea quidem quaerat totam odio! Animi fuga numquam, veritatis illo nulla commodi repellendus modi omnis!
+Molestiae sint laboriosam repudiandae quia architecto nesciunt sapiente dolore ullam autem dolorum veniam dolorem laborum consectetur animi, accusamus recusandae esse unde suscipit possimus ipsum ab voluptatibus placeat, provident atque! Perferendis!
+Ab dolor, doloribus laborum ipsam vitae optio, incidunt totam, provident tenetur animi rem vel corrupti repudiandae nam esse inventore eius asperiores et amet magni. Numquam quis deserunt earum ipsa laudantium.
+Officia accusantium neque iusto quia totam dolorem sed alias voluptatibus veritatis est nemo incidunt inventore repudiandae officiis cum, itaque rem! Architecto velit ad molestias sequi! Ipsam ducimus unde expedita cum?
+Sint delectus expedita hic ratione, neque ex. Neque, ad. Quam alias est ipsum autem, neque consequuntur ex eius totam. Unde magni aspernatur nemo doloremque. Dolores distinctio harum saepe similique id.
+<h2>Aliquam incidunt vitae ullam similique tenetur consequuntur. Facilis repellat eligendi, recusandae corrupti impedit laborum eveniet asperiores quaerat nobis quae perferendis esse reprehenderit nihil molestiae suscipit placeat maiores necessitatibus quos excepturi.</h2>`;
+var productsContent =  `<h1>Products Page</h1><h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nesciunt repudiandae quibusdam? Quia inventore, reprehenderit omnis neque eum sunt excepturi, iure facilis eveniet aut ratione repellendus exercitationem culpa! Expedita, odit.</h2><h3>Commodi facilis numquam aliquam itaque sint officiis error hic veritatis! Laboriosam architecto dolor inventore quia cupiditate provident adipisci quis nemo consequatur. Veritatis labore natus accusantium esse molestiae architecto ex eos!</h3>`;
+var contactContent =  `<h1>Contact Page</h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolore odio odit reiciendis ipsam dolorum repellat architecto nostrum consequatur quod ducimus quas vel, et accusamus rerum? Sequi neque accusantium dolor.
+Delectus, debitis quos! Facilis nostrum debitis nihil quidem expedita deserunt, voluptates quaerat, recusandae atque labore voluptatum. Architecto vero quisquam nisi in esse aperiam, id minus! Mollitia id necessitatibus sunt libero.
+Non, facilis quae et dolor alias sunt laboriosam ipsam ducimus, ea nesciunt dignissimos nisi magni perspiciatis omnis assumenda? Rem natus consectetur eaque vero explicabo qui a optio aliquid quo magnam.
+Deserunt totam explicabo dolores eligendi quaerat repudiandae fugiat perferendis qui ducimus nulla suscipit dolorem, nihil corporis dignissimos. Quaerat quibusdam facere debitis! Eius explicabo sapiente iusto perspiciatis, quibusdam totam labore maiores?
+Aut asperiores temporibus quae quasi sunt. Alias voluptate vero explicabo doloremque earum at neque pariatur eligendi facere animi sit repellendus dolores placeat officiis aperiam suscipit voluptates, necessitatibus dolorum sint totam.
+Omnis atque adipisci tempora veritatis quod dicta voluptate error dolore saepe minus, voluptatum magni labore praesentium odio culpa non explicabo facilis incidunt laudantium aliquid ut nam asperiores. Iste, quasi magnam?
+Ullam laudantium eaque deleniti, perspiciatis adipisci fugiat consectetur amet placeat alias enim sint voluptates animi libero qui dolores consequatur soluta recusandae explicabo necessitatibus molestias perferendis vero quibusdam! Maxime, repellat ratione?
+Natus officia obcaecati dolore. Optio, reprehenderit. Accusamus magni nisi quidem distinctio earum ratione odio pariatur voluptas eius ex mollitia voluptatem, suscipit rem, libero ab! Veniam aut nisi magnam iusto dolorum.
+Molestiae enim eaque ea voluptatem dignissimos hic recusandae ipsum? Ipsum odit, in itaque adipisci quod quam nesciunt fugiat reprehenderit a illum vel, sed cumque, doloribus repellendus molestias voluptates iure animi?
+Asperiores placeat voluptates id dignissimos. Culpa est, cum corrupti aspernatur suscipit esse maxime saepe id aut hic illum fugiat quia sed perspiciatis labore! Modi aut incidunt quod excepturi placeat veniam.
+A atque dolor neque nulla eos alias consequatur voluptatem, iste laboriosam, voluptates, pariatur ipsa quaerat illum nesciunt esse tenetur. Sunt ea fugiat nam laborum ex accusamus optio similique? Voluptatem, obcaecati?
+Laboriosam modi sapiente officia recusandae dolores porro amet, iure illum blanditiis ratione hic neque suscipit earum cumque quod nihil reiciendis! Explicabo eum, adipisci quos sequi saepe dolor aliquam fugiat labore.
+Aliquam nisi ad fugit non unde velit nobis nostrum alias quam error? Consequuntur nemo maiores, nulla eum quo nostrum dolorem repellat voluptatem eius numquam molestias, nam, corporis architecto et suscipit!
+Molestiae ea esse tenetur dolore corrupti iste? Tenetur assumenda at ab nulla similique sapiente tempore culpa numquam officia quos debitis, dolores enim odio quo quae saepe, pariatur amet? Cupiditate, et.
+Quas id cum magni consequatur nemo porro commodi! Assumenda ea dolorum perspiciatis eos! Ducimus optio eius alias impedit sapiente ipsam, explicabo a voluptate dolore hic in esse porro, distinctio dolorem.
+In delectus consequuntur tempora impedit, obcaecati incidunt provident autem, ipsa aspernatur ducimus saepe adipisci excepturi sapiente. Ea autem doloribus tenetur, cum odit deleniti fugiat! Eveniet cumque commodi quasi rem unde.
+Ex labore delectus tempora adipisci ab porro dolores dolore, asperiores inventore libero esse ipsum sequi repellendus, nesciunt voluptates accusamus assumenda aut, hic ipsa voluptas magnam. Dolore deleniti rerum error asperiores!
+Dicta dignissimos velit quae odit, optio eaque repellendus facere voluptatibus quasi doloremque beatae illum deserunt. Quam perspiciatis dolore officiis saepe laboriosam ut officia, nobis sint, veritatis possimus obcaecati! Culpa, magni.
+Provident rerum expedita sit enim nihil. Corporis, dolore ipsa optio modi voluptas perspiciatis aliquid reprehenderit corrupti saepe dolorum nulla similique ut consectetur voluptatibus in animi excepturi iure tempora hic ad?
+Repudiandae sapiente earum a reiciendis saepe explicabo nihil aliquid necessitatibus hic dolore esse perferendis id nam, neque ipsa quod repellendus unde ratione rem molestias voluptatibus architecto. Officiis molestias modi iusto.`;
+
+
+export function addPageContent(pageName) {
+    let pageContentName =  pageName + "Content";
+    $("#app").html(eval(pageContentName));
+}
+
+export function setLoggedInOut (){
+    if(loggedInOut == true){
+        loggedInOut = false;
+        $("nav .log span").html("log in")
+    } else {
+        loggedInOut = true;
+        $("nav .log span").html("log out")
+    }
+}
+
+export function getLoggedInOut(){
+    return loggedInOut;
+}
+
+
